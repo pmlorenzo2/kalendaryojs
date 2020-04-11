3036ad627d57d44bd25f565db4e122bfeda5f7f2
Miguel Lorenzo <pmlorenzo2@gmail.com>
2020-04-11 21:52:12 +0800

    Made some updates with the src file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,37 @@
 /**
  * Zavin Datepicker -> src -> Index JS
  */
+
+
+const render = async (param) => {
+
+
+    // ---------------------------------------
+    // 🐬. Tasks before render
+    // ---------------------------------------
+
+    //- VALIDATE: [param] - Required
+    if (!param) throw new Error("param is required");
+    if (typeof param !== "object") throw new Error("param must be an object");
+
+    //- VALIDATE: [param.id] - Required
+    if (!param.id) throw new Error("param.id is required");
+    if (typeof param.id !== "string") throw new Error("param.id must be a string");
+
+    //- VALIDATE: [param.label] - Optional
+    if (param.label && typeof param.label !== "string") throw new Error("param.label must be a string");
+
+
+    // ---------------------------------------
+    // 🐇. Render datepicker here
+    // ---------------------------------------
+
+
+    // ---------------------------------------
+    // 🦋. Event handlers here
+    // ---------------------------------------
+};
+
+
+// Expose
+export default { render };
