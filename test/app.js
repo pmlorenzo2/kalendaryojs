6d3b4b37e096d096bd88ecddefb3979a61819912
Miguel Lorenzo <pmlorenzo2@gmail.com>
2020-04-12 06:04:15 +0800

    Made some updates with the test

--- test/app.js
+++ test/app.js
@@ -1,1 +1,10 @@
 // app.js
+import ZavinDatepicker from "../src/index.js";
+
+ZavinDatepicker.render({
+    id: "from",
+    label: "From",
+    pickerIcon: "./icons/icons8-calendar-100.png",
+    prevIcon: "./icons/icons8-back-100.png",
+    nextIcon: "./icons/icons8-forward-100.png"
+});
