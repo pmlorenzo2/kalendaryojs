07354c1088a9fd47dd341536823d34ce3d090b05
Miguel Lorenzo <pmlorenzo2@gmail.com>
2020-04-12 00:46:56 +0800

    Made some updates with the src file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@
  */
 
 
+const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+
+// Define number of items to be generated in selection year
+const selection_year_count = 10;
+
 const render = async (param) => {
+    let currentYear, currentMonth, currentDate;
 
 
     // ---------------------------------------
@@ -22,10 +29,31 @@ const render = async (param) => {
     //- VALIDATE: [param.label] - Optional
     if (param.label && typeof param.label !== "string") throw new Error("param.label must be a string");
 
+    // Use date today as initial for current values
+    const today = new Date();
+    currentYear = today.getFullYear();
+    currentMonth = today.getMonth();
+    currentDate = today.getDate();
+
 
     // ---------------------------------------
     // 🐇. Render datepicker here
     // ---------------------------------------
+    let labelElement;
+    if (param.label) labelElement = `<h6 class="has-text-weight-bold">${param.label}</h6>`;
+
+    document.getElementById(param.id).innerHTML = `
+        <div class="dropdown zavin-datepicker" id="${param.id}Datepicker">
+            <a href="javascript:void(0)" class="dropdown-trigger" id="${param.id}DatepickerTrigger">
+                <div class="has-margin-right-4">
+                    ${labelElement}
+                    <p id="${param.id}DatepickerDate">${months[currentMonth].substring(0, 3)}. ${currentDate}, ${currentYear}</p>
+                </div>
+                <figure class="image is-24x24"></figure>
+            </a>
+            <div class="dropdown-content"></div>
+        </div>
+    `;
 
 
     // ---------------------------------------
