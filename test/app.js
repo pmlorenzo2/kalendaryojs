dc3c8b253b8afeb278fdad10bcdb197b2f9aa963
Miguel Lorenzo <pmlorenzo2@gmail.com>
2020-04-13 03:49:46 +0800

    Made some updates with the test file

--- test/app.js
+++ test/app.js
@@ -8,3 +8,19 @@ ZavinDatepicker.render({
     prevIcon: "./icons/icons8-back-100.png",
     nextIcon: "./icons/icons8-forward-100.png"
 });
+
+ZavinDatepicker.render({
+    id: "to",
+    label: "To",
+    pickerIcon: "./icons/icons8-calendar-100.png",
+    prevIcon: "./icons/icons8-back-100.png",
+    nextIcon: "./icons/icons8-forward-100.png"
+});
+
+document.onclick = () => {
+    const fromDatepicker = document.getElementById("fromDatepicker");
+    if (fromDatepicker) fromDatepicker.classList.remove("is-active");
+
+    const toDatepicker = document.getElementById("toDatepicker");
+    if (toDatepicker) toDatepicker.classList.remove("is-active");
+};
