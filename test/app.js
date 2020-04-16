218bb8829c124bcf94b3c24192431a8889953f3a
Miguel Lorenzo <pmlorenzo2@gmail.com>
2020-04-17 02:56:38 +0800

    Made some updates with the test file

--- test/app.js
+++ test/app.js
@@ -7,7 +7,8 @@ ZavinDatepicker.render({
     pickerIcon: "./icons/icons8-calendar-100.png",
     option: {
         prevIcon: "./icons/icons8-back-100.png",
-        nextIcon: "./icons/icons8-forward-100.png"
+        nextIcon: "./icons/icons8-forward-100.png",
+        today: true
     }
 });
 
@@ -17,7 +18,8 @@ ZavinDatepicker.render({
     pickerIcon: "./icons/icons8-calendar-100.png",
     option: {
         prevIcon: "./icons/icons8-back-100.png",
-        nextIcon: "./icons/icons8-forward-100.png"
+        nextIcon: "./icons/icons8-forward-100.png",
+        today: true
     }
 });
 
