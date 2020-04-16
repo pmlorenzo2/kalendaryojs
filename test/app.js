5349d909f278a761ccf04ec08f55619d02978f40
Miguel Lorenzo <pmlorenzo2@gmail.com>
2020-04-17 02:31:51 +0800

    Made some changes with the test

--- test/app.js
+++ test/app.js
@@ -5,16 +5,20 @@ ZavinDatepicker.render({
     id: "from",
     label: "From",
     pickerIcon: "./icons/icons8-calendar-100.png",
-    prevIcon: "./icons/icons8-back-100.png",
-    nextIcon: "./icons/icons8-forward-100.png"
+    option: {
+        prevIcon: "./icons/icons8-back-100.png",
+        nextIcon: "./icons/icons8-forward-100.png"
+    }
 });
 
 ZavinDatepicker.render({
     id: "to",
     label: "To",
     pickerIcon: "./icons/icons8-calendar-100.png",
-    prevIcon: "./icons/icons8-back-100.png",
-    nextIcon: "./icons/icons8-forward-100.png"
+    option: {
+        prevIcon: "./icons/icons8-back-100.png",
+        nextIcon: "./icons/icons8-forward-100.png"
+    }
 });
 
 document.onclick = () => {
